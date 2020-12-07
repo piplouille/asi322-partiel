3e72fbba69703134992bedf620894adfddd9e3be
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-12-07 12:02:24 +0100

    credentials twitter

--- test_api.py
+++ test_api.py
@@ -4,6 +4,14 @@
 #                   access_token_key="AAAAAAAAAAAAAAAAAAAAAD0nKQEAAAAA3W3vlvuDaoP3ZHiff6%2BgaebX4PU%3DS8MtsU2fmrymJ3PHoOhqF5dtrMl9A7meddAoUoHvCL0zy0DJZv",
 #                   access_token_secret="")
 
-import requests
-r = requests.get('https://api.twitter.com/1.1/collections/entries.json?id=custom-539487832448843776')
-print(r.json())
+# import requests
+# r = requests.get('https://api.twitter.com/1.1/collections/entries.json?id=custom-539487832448843776')
+# print(r.json())
+
+# python3 test_api.py
+
+from searchtweets import load_credentials
+
+load_credentials(filename="./search_tweets_creds_example.yaml",
+                 yaml_key="search_tweets_api",
+                 env_overwrite=False)
