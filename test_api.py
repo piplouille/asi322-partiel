d478328a549dc01e7e37d63c5a6d67564dc3f430
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-12-07 12:10:31 +0100

    recherche de tweets

--- test_api.py
+++ test_api.py
@@ -10,8 +10,16 @@
 
 # python3 test_api.py
 
-from searchtweets import load_credentials
+from searchtweets import load_credentials, ResultStream, gen_rule_payload, collect_results
 
-load_credentials(filename="./search_tweets_creds_example.yaml",
+
+premium_search_args = load_credentials(filename="./search_tweets_creds_example.yaml",
                  yaml_key="search_tweets_api",
                  env_overwrite=False)
+
+rule = gen_rule_payload("beyonce", results_per_call=100) # testing with a sandbox account
+print(rule)
+
+tweets = collect_results(rule, max_results=100, result_stream_args=premium_search_args)
+
+[print(tweet.all_text, end='\n\n') for tweet in tweets[0:10]]; 
