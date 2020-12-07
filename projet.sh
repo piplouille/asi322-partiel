763bf972e03a552a44b9d72d55f4e2e16b2b2338
DEBESSE Laetitia <laetitia.debesse@ensta-paristech.fr>
2020-12-07 12:29:58 +0100

    correctif script

--- projet.sh
+++ projet.sh
@@ -1,3 +1,3 @@
 ./apache-zookeeper-3.6.2-bin/bin/zkServer.sh start
 cd kafka_2.12-2.2.0/bin
-gnome-terminal -e "./kafka-server-start.sh ../config/server.properties" --
+./kafka-server-start.sh ../config/server.properties
